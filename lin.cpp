cb5d09bb02f7124506df8b2eb075e772b3c9d7d9
Nate <Nate@pelmel.net>
2023-09-16 03:45:11 -0500

    Add files via upload

---
+++ lin.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+double num1;
+double num2;
+double result;
+char op;
+int main() {
+cout << "**********Calculator**********\n";
+cout << "(+,-,*,/):";
+cin >> op;
+cout << "Number One:\n";
+cin >> num1;
+cout << "Number Two:\n";
+cin >> num2;
+switch (op)
+{
+case '+':
+    result = num1 + num2;
+    cout << "is: " << result;
+    break;
+case '-':
+    result = num1 - num2;
+    cout << "is: " << result;
+    break;
+case '*':
+    result = num1 * num2;
+    cout << "is: " << result;
+    break;
+case '/':
+    result = num1 / num2;
+    cout << "is: " << result;
+    break;
+default:
+    cout << "Error: Unknown operator";
+    break;
+}
+system("sleep 2s");
+cout << "\n******************************";
+return 0;
+}
